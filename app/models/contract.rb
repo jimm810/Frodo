91e655de87bf5b1d5ac4ae4bece68dd0a785c287
Jim McGrath <jimmcgrath25@gmail.com>
2011-04-09 23:09:10 -0400

    done with db fixes for contract type

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,6 +1,6 @@
 class Contract < ActiveRecord::Base
   attr_accessible :name, :contract_type, :execution_date, :customer_id, :active
-  
+     
   belongs_to :customer
   has_many :contract_details
   
